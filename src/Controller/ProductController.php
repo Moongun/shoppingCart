3bb3cc91f4d09a9a2af89f623a17a16b3b2eb943
Moongun <mich.wiaz@gmail.com>
2019-01-22 22:34:43 +0100

    fix namespace

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,16 +1,6 @@
 <?php
+namespace App\Controller;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of ProductController
- *
- * @author michal
- */
 class ProductController {
     //put your code here
 }
