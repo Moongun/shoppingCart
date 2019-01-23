6e4cc9eae27a3119a8286bd4b3d35f5ec4ad5bce
Moongun <mich.wiaz@gmail.com>
2019-01-23 22:20:46 +0100

    crud actions for Product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,13 +2,89 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Form\ProductType;
 
-class ProductController extends AbstractController{
+class ProductController extends AbstractController {
 
     public function index()
     {
-        return $this->render('Product/list.html.twig', ['param' => 'Hey miszczu']);
+        $entityManager = $this->getDoctrine()->getManager();
+        $products = $entityManager->getRepository(Product::class)->findAll();
+
+        return $this->render('Product/index.html.twig', ['products' => $products ]);
     }
     
+    public function create(Request $request)
+    {
+        $product = new Product();
+        
+        $form = $this->createForm(ProductType::class, $product);
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            
+            try {
+                $entityManager->persist($product);
+                $entityManager->flush();    
+            } catch (Exception $ex) {
+                throw $ex->message();
+            }
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('Product/edit.html.twig', ['form' => $form->createView()]);
+    }
+    
+    public function edit(Request $request, $id) 
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+        
+        if(!$product) {
+            throw $this->createNotFoundException("Not found product with id: $id");
+        }
+        
+        $form = $this->createForm(ProductType::class, $product);
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            
+            try {
+                $entityManager->persist($product);
+                $entityManager->flush();    
+            } catch (Exception $ex) {
+                throw $ex->message();
+            }
+            return $this->redirectToRoute('product_index');
+        }
+        
+        return $this->render('Product/edit.html.twig', ['form' => $form->createView()]);
+    }
+    
+    public function delete($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if(!$product) {
+            throw $this->createNotFoundException("Not found product with id: $id");
+        }
+        
+        $entityManager->remove($product);
+        $entityManager->flush();
+        
+        $this->redirectToRoute('product_index', []);
+    }
 }
